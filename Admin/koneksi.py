db097e0d5baeeff0aedf0544eee54ddcde8753ae
legikuswandi <legi2807@upi.edu>
2024-11-03 22:01:43 +0700

    admin add some file

--- Admin/koneksi.py
+++ Admin/koneksi.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['utsolshop']
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
